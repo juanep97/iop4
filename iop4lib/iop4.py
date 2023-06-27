d7bfaf94339a86434b0de9c26dacaadf26b84e9e
Juan Escudero Pedrosa <jescudero@iaa.es>
2023-06-27 18:59:58 +0200

    remove outdated docstring in iop4.py script

--- iop4lib/iop4.py
+++ iop4lib/iop4.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 """ iop4.py
     Invoke the IOP4 pipeline.
-
-    This script should be invoked with the epoch you want to reduce (e.g. TEL/YYMMDD).
-    Then it should download the data if not already downloaded, and reduce it.
-
-    $ rm ~/iop4data/iop4.db; rm -rf ./iop4api/migrations; python manage.py flush; python manage.py makemigrations iop4api; python manage.py migrate; python manage.py loaddata ../iop4lib/db/flags.json
-    $ python manage.py dumpdata --natural-primary --natural-foreign --format=yaml
 """
 
 import iop4lib.config
@@ -39,6 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--log-level', type=str, dest='log_level', choices=[None, 'debug', 'info', 'error', 'warning', 'critical'], default=None, help='Logging level to use (default: %(default)s)')   
     parser.add_argument('-m', '--mail-to', dest='mail_to', default=list(), nargs='+', help='<Optional> List of email addresses to send the log to', required=False)
     parser.add_argument("--nthreads", dest="nthreads", type=int, default=None, help="<Optional> Number of threads to use when possible (default: %(default)s)", required=False)
+    parser.add_argument("--use-ray", dest="use_ray", action="store_true", help="<Optional> Use ray for parallelization", required=False)
     
     parser.add_argument('--retry-failed', dest='retry_failed', action='store_true', help='<Optional> Retry failed reduced fits', required=False)
     parser.add_argument('-l', '--epoch-list', dest='epochname_list', nargs='+', help='<Optional> List of epochs (e.g: T090/230102 T090/230204)', required=False)
