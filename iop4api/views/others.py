46e4419459d0e603c81c06c0bbe3f2a4cd52b14d
Juan Escudero Pedrosa <jescudero@iaa.es>
2023-09-27 19:31:57 +0200

    human friendly column headers in web portal

--- iop4api/views/others.py
+++ iop4api/views/others.py
@@ -78,7 +78,19 @@ def logout_view(request):
 @permission_required(["iop4api.view_astrosource"])
 def catalog(request):
     qs = AstroSource.objects.exclude(srctype=SRCTYPES.CALIBRATOR).exclude(srctype=SRCTYPES.UNPOLARIZED_FIELD_STAR).values()
-    return JsonResponse({'data': list(qs)})
+
+    data = list(qs)
+
+    if len(data) > 0:
+        all_column_names = data[0].keys()
+        default_column_names = set(all_column_names)
+    else:
+        all_column_names = []
+        default_column_names = []
+
+    columns = [{"name": k, "title": AstroSource._meta.get_field(k).verbose_name, "field":k, "visible": (k in default_column_names)} for k in all_column_names]
+
+    return JsonResponse({'data': data, 'columns': columns})
 
 
 
@@ -100,9 +112,9 @@ def data(request):
         all_column_names = []
         default_column_names = []
 
-    columns = [{"title": k, "field":k, "visible": (k in default_column_names)} for k in all_column_names]
+    columns = [{"name": k, "title": PhotoPolResult._meta.get_field(k).verbose_name, "field":k, "visible": (k in default_column_names)} for k in all_column_names]
 
-    return JsonResponse({'data': list(vals), 'tabulatorjs_columns': columns})
+    return JsonResponse({'data': list(vals), 'columns': columns})
 
 
 
