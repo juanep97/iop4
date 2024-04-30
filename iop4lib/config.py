1d89c3281ce01f7ceeeb4359694e9f0a2c5e4f15
Juan Escudero <jescudero@iaa.es>
2024-04-30 16:58:06 +0000

    code style

--- iop4lib/config.py
+++ iop4lib/config.py
@@ -1,7 +1,7 @@
 # other imports
 import os, yaml, logging
 from pathlib import Path
-from importlib.resources import files
+from importlib import resources
 import matplotlib, matplotlib.pyplot
 
 # Disable matplotlib logging except for warnings and above
@@ -113,7 +113,7 @@ class Config(dict):
                 elif Path("~/.iop4.config.yaml").expanduser().exists():
                     config_path = Path("~/.iop4.config.yaml").expanduser()
                 else:
-                    config_path = files("iop4lib") / "config.example.yaml"
+                    config_path = resources.files("iop4lib") / "config.example.yaml"
 
                 if not config_path.exists():
                     raise FileNotFoundError(f"Config file {config_path} not found.")
@@ -232,7 +232,7 @@ class Config(dict):
         with open(self.config_path, 'r') as f:
             config_dict = yaml.safe_load(f)
 
-        with open(files("iop4lib") / "config.example.yaml", 'r') as f:
+        with open(resources.files("iop4lib") / "config.example.yaml", 'r') as f:
             config_dict_example = yaml.safe_load(f)
 
         wrong = False
