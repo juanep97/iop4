d4d71e48bbb7150e9c6db239fff53f5b65b0c419
Juan Escudero <jescudero@iaa.es>
2024-09-18 10:11:23 +0000

    add basic auto-flagging

--- iop4lib/iop4.py
+++ iop4lib/iop4.py
@@ -79,6 +79,13 @@ def process_epochs(epochname_list: Iterable[str], args):
         epoch.compute_relative_photometry()
         epoch.compute_relative_polarimetry()
 
+    logger.info("Auto-flagging points.")
+    
+    for result in PhotoPolResult.objects.filter(epoch__in=epoch_L).all():
+        if result.p is not None and not (0 <= result.p <= 1):
+            result.flags.add(PhotoPolResult.FLAGS.ERROR_POLARIMETRY)
+            result.save()
+
     logger.info("Applying corrections.")
 
     for epoch in epoch_L:
