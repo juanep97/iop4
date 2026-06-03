a83b4a92259f195bb9f1ca7669f38a963cf1ebfc
Juan Escudero <jescudero@iaa.es>
2026-06-03 18:56:09 +0000

    code style

--- iop4lib/db/astrosource.py
+++ iop4lib/db/astrosource.py
@@ -290,7 +290,7 @@ class AstroSource(models.Model):
             metadata_value = yaml.safe_load(self.metadata_str)
         except yaml.YAMLError as e:
             raise ValidationError({
-                "metadata_str": f"Invalid YAML field metadata_str: {e}"
+                "metadata_str": f"Invalid YAML field metadata_str: {e}",
             })
         self.validate_metadata(metadata_value or dict())
 
